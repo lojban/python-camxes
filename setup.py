a2967d9c47362bc53db6ad5eaafe0695183d576b
Dag Odenhall <dag.odenhall@gmail.com>
2011-03-28 17:23:04 +0200

    Packagify

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='camxes',
       version='0.1',
+      packages=find_packages(),
       install_requires=['lepl'],
       tests_require=['attest>=0.5'],
       test_loader='attest:auto_reporter.test_loader',
-      test_suite='tests.all',
+      test_suite='camxes.tests.all',
      )
