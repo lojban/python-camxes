068e87995a2751097e744355d2f11b537b5e51c8
Dag Odenhall <dag.odenhall@gmail.com>
2011-03-28 22:36:19 +0200

    Prepare for release

--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
 from setuptools import setup, find_packages
 
-setup(name='camxes',
-      version='0.1',
-      packages=find_packages(),
-      package_data={'camxes': ['camxes.jar']},
-      install_requires=['lepl'],
-      tests_require=['attest>=0.5'],
-      test_loader='attest:auto_reporter.test_loader',
-      test_suite='camxes.tests.all',
-      use_2to3=True,
-     )
+setup(
+    name='camxes',
+    version='0.1',
+    description='Python interface to camxes.',
+    long_description=open('README.rst').read(),
+
+    author='Dag Odenhall',
+    author_email='dag.odenhall@gmail.com',
+    license='Simplified BSD',
+    url='https://github.com/dag/python-camxes',
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['lepl'],
+    zip_safe=False,
+
+    tests_require=['attest>=0.5'],
+    test_loader='attest:auto_reporter.test_loader',
+    test_suite='camxes.tests.all',
+    use_2to3=True,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Java',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+)
