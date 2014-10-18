86eb807fe87366f1b28bbcc2ac80430d9fcbf070
Riley Martinez-Lynch <oss+lojban@teleological.net>
2014-10-18 14:48:25 -0300

    v0.3: Upgraded jar to use newer BPFK morphology
    * camxes.jar built from maven branch of lojban/camxes @ 80d3474
    * morphology upgraded from v108 to v119 (October 2009)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='camxes',
-    version='0.2',
+    version='0.3',
     description='Python interface to camxes.',
     long_description=open('README.rst').read(),
 
